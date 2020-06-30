5b135562c06ebb8cfcb5131750ec25aae91fada8
lienzhu <56902061+lienzhu@users.noreply.github.com>
2020-06-30 23:39:22 +1000

    API calls - Working Version 1

--- app/src/main/java/com/example/vactracker/ui/DataService.java
+++ app/src/main/java/com/example/vactracker/ui/DataService.java
@@ -1,14 +1,10 @@
 package com.example.vactracker.ui;
 
-import com.example.vactracker.ui.data.Data;
 import com.example.vactracker.ui.data.Vaccine;
 
 import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
