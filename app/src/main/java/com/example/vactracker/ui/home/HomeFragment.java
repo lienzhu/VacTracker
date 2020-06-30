785bc91c2e5ade01f42d1ff3d7e629a37ef49fe7
lienzhu <56902061+lienzhu@users.noreply.github.com>
2020-06-30 13:51:29 +1000

    Initial Setup

--- app/src/main/java/com/example/vactracker/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/vactracker/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.vactracker.ui.home;
 
+import android.app.Application;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +13,28 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.vactracker.R;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private TextView text_home;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -30,6 +45,53 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+//        final JSONObject jsonObject = new JSONObject();
+//        try{
+//            jsonObject.put("")
+//        }
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        String url ="https://api.c3.ai/covid/api/1/therapeuticasset/fetch";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+
+                        textView.setText(("Response is: "+ response.substring(0,500)));
+                        //textView.setText("Test Successful");
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        })
+
+//        {
+//            @Override
+//            protected Map<String, String> getParams()
+//            {
+//                Map<String, String>  params = new HashMap<String, String>();
+//                params.put("filter", "therapyType == 'Vaccine'");
+//                return params;
+//            }
+//        }
+                ;
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
+
         return root;
     }
+
+
+
+
 }
