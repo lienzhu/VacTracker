f4d08734e47358c3b0827b15c69136b855790640
Jonson <jonson0211@hotmail.com>
2020-07-21 00:58:02 +1000

    search function

--- app/src/main/java/com/example/vactracker/AppDatabase.java
+++ app/src/main/java/com/example/vactracker/AppDatabase.java
@@ -7,7 +7,7 @@ import androidx.room.TypeConverters;
 import com.example.vactracker.DAO.ObjDAO;
 import com.example.vactracker.ui.data.Obj;
 
-@Database(entities = {Obj.class}, version = 2)
+@Database(entities = {Obj.class}, version = 3)
  @TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
 
--- app/src/main/java/com/example/vactracker/ui/vaccines/VaccinesFragment.java
+++ app/src/main/java/com/example/vactracker/ui/vaccines/VaccinesFragment.java
@@ -1,27 +1,26 @@
 package com.example.vactracker.ui.vaccines;
 
-import android.bluetooth.le.PeriodicAdvertisingParameters;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.view.inputmethod.EditorInfo;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import com.example.vactracker.AppDatabase;
-import com.example.vactracker.DAO.ObjDAO;
 import com.example.vactracker.R;
 import com.example.vactracker.adapters.VaccineAdapter;
 import com.example.vactracker.ui.DataService;
@@ -29,13 +28,11 @@ import com.example.vactracker.ui.data.Obj;
 import com.example.vactracker.ui.data.Vaccine;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -48,6 +45,7 @@ public class VaccinesFragment extends Fragment {
     private AppDatabase mDb;
     private RecyclerView mRvList;
 
+
     public static final Integer EXTRA_MESSAGE = 0;
     DataService service;
     @Override
@@ -56,6 +54,8 @@ public class VaccinesFragment extends Fragment {
 
         mDb = Room.databaseBuilder(getContext(), AppDatabase.class, "app-database").allowMainThreadQueries().fallbackToDestructiveMigration()
                 .build();
+
+
 
         //getting values from DB
         new GetObjDBTask().execute();
@@ -71,6 +71,7 @@ public class VaccinesFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_vaccines, container, false);
 
          mRvList = root.findViewById(R.id.list);
+
         mRvList.setLayoutManager(new LinearLayoutManager(getContext()));
         VaccineAdapter.RecyclerViewClickListener listener = new VaccineAdapter.RecyclerViewClickListener() {
             @Override
@@ -82,6 +83,7 @@ public class VaccinesFragment extends Fragment {
         vaccineAdapter = new VaccineAdapter(mDb.objDAO().getObjs(),listener);
 
         mRvList.setAdapter(vaccineAdapter);
+        setHasOptionsMenu(true);
 
         return root;
     }
@@ -145,5 +147,26 @@ public class VaccinesFragment extends Fragment {
         intent.putExtra(String.valueOf(EXTRA_MESSAGE), position);
         startActivity(intent);
     }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater){
+       menuInflater.inflate(R.menu.search_menu,menu);
+       MenuItem searchItem = menu.findItem(R.id.action_search);
+       SearchView searchView = (SearchView) searchItem.getActionView();
+       searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+       searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+           @Override
+           public boolean onQueryTextSubmit(String query) {
+               return false;
+           }
+
+           @Override
+           public boolean onQueryTextChange(String newText) {
+               vaccineAdapter.getFilter().filter(newText);
+               return false;
+           }
+       });
+        super.onCreateOptionsMenu(menu,menuInflater);
+    }
+
 
 }
